9d2177747e2b61c23035a88fde1d9e8508e62488
Henrique Borges <henriqueborgeshbr@gmail.com>
2021-02-15 01:07:56 -0300

    feat(front-vanilla): make article section responsive

--- packages/front-vanilla/public/build/slider.js
+++ packages/front-vanilla/public/build/slider.js
@@ -1,5 +1,15 @@
 const $$ = document.querySelectorAll.bind(document)
 
+const sliderResponsivity = flkty => {
+  if (window.innerWidth <= 720) {
+    $$('.slider-cell').forEach(sliderCell => sliderCell.classList.add('mobile'))
+    flkty.reloadCells()
+  } else if (document.querySelectorAll('.slider-cell.mobile')) {
+    $$('.slider-cell').forEach(sliderCell => sliderCell.classList.remove('mobile'))
+    flkty.reloadCells()
+  }
+}
+
 export const slider = () => {
   const sliderElement = document.querySelector('#slider')
   const flkty = new Flickity(sliderElement, {
@@ -7,13 +17,5 @@ export const slider = () => {
     cellAlign: 'center'
   })
 
-  window.onresize = () => {
-    if (window.innerWidth <= 720) {
-      $$('.slider-cell').forEach(sliderCell => sliderCell.classList.add('mobile'))
-      flkty.reloadCells()
-    } else if (document.querySelectorAll('.slider-cell.mobile')) {
-      $$('.slider-cell').forEach(sliderCell => sliderCell.classList.remove('mobile'))
-      flkty.reloadCells()
-    }
-  }
+  window.onresize = sliderResponsivity(flkty)
 }
