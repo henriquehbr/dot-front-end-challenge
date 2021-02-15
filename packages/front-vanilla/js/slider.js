f2654b0a6f3a507c7f0a1c8b51726b5ec49f8f33
Henrique Borges <henriqueborgeshbr@gmail.com>
2021-02-15 01:36:29 -0300

    chore(front-vanilla): wrap `slideResponsivity` with arrow function

--- packages/front-vanilla/js/slider.js
+++ packages/front-vanilla/js/slider.js
@@ -3,10 +3,10 @@ const $$ = document.querySelectorAll.bind(document)
 const sliderResponsivity = flkty => {
   if (window.innerWidth <= 720) {
     $$('.slider-cell').forEach(sliderCell => sliderCell.classList.add('mobile'))
-    flkty.reloadCells()
+    flkty.resize()
   } else if (document.querySelectorAll('.slider-cell.mobile')) {
     $$('.slider-cell').forEach(sliderCell => sliderCell.classList.remove('mobile'))
-    flkty.reloadCells()
+    flkty.resize()
   }
 }
 
@@ -17,5 +17,5 @@ export const slider = () => {
     cellAlign: 'center'
   })
 
-  window.onresize = sliderResponsivity(flkty)
+  window.onresize = () => sliderResponsivity(flkty)
 }
