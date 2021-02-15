b37cba6b16ebc78fde7c80fc6df61e53f214d8aa
Henrique Borges <henriqueborgeshbr@gmail.com>
2021-02-14 23:08:26 -0300

    feat(front-vanilla): create accordions section

---
+++ packages/front-vanilla/js/accordions.js
@@ -0,0 +1,30 @@
+const $$ = document.querySelectorAll.bind(document)
+
+const closeAllAccordions = accordionHeaders =>
+  accordionHeaders.forEach(accordionHeader => {
+    accordionHeader.classList.remove('open')
+    accordionHeader.nextElementSibling.classList.remove('open')
+    accordionHeader.querySelector('i').classList.remove('open')
+  })
+
+const toggleAccordion = accordionHeader => {
+  if (accordionHeader.classList.contains('open')) {
+    accordionHeader.classList.remove('open')
+    accordionHeader.nextElementSibling.classList.remove('open')
+    accordionHeader.querySelector('i').classList.remove('open')
+  } else {
+    accordionHeader.classList.add('open')
+    accordionHeader.nextElementSibling.classList.add('open')
+    accordionHeader.querySelector('i').classList.add('open')
+  }
+}
+
+export const accordions = () => {
+  const accordionHeaders = $$('.accordion-header')
+  accordionHeaders.forEach(accordionHeader => {
+    accordionHeader.addEventListener('click', ({ target }) => {
+      closeAllAccordions(accordionHeaders)
+      toggleAccordion(target)
+    })
+  })
+}
--- packages/front-vanilla/public/build/index.js
+++ packages/front-vanilla/public/build/index.js
@@ -1,3 +1,5 @@
 import { slider } from './slider.js'
+import { accordions } from './accordions.js'
 
 slider()
+accordions()
