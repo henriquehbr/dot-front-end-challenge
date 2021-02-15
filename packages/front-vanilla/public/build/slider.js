1a5be7bfa8e304e1ff2f8fca0f7290f82f0aa6cf
Henrique Borges <henriqueborgeshbr@gmail.com>
2021-02-15 00:51:09 -0300

    feat(front-vanilla): make slider responsive

--- packages/front-vanilla/public/build/slider.js
+++ packages/front-vanilla/public/build/slider.js
@@ -1,8 +1,19 @@
+const $$ = document.querySelectorAll.bind(document)
+
 export const slider = () => {
   const sliderElement = document.querySelector('#slider')
-  new Flickity(sliderElement, {
+  const flkty = new Flickity(sliderElement, {
     pageDots: false,
-    cellAlign: 'center',
-    groupCells: 3
+    cellAlign: 'center'
   })
+
+  window.onresize = () => {
+    if (window.innerWidth <= 720) {
+      $$('.slider-cell').forEach(sliderCell => sliderCell.classList.add('mobile'))
+      flkty.reloadCells()
+    } else if (document.querySelectorAll('.slider-cell.mobile')) {
+      $$('.slider-cell').forEach(sliderCell => sliderCell.classList.remove('mobile'))
+      flkty.reloadCells()
+    }
+  }
 }
